f73579f57c65962f5234555eb9dbb011ce2f0442
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-12 15:49:04 +0100

    Set up search bar component and error message

--- src/context.js
+++ src/context.js
@@ -8,7 +8,7 @@ const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState({ show: false, msg: "" });
   const [movies, setMovies] = useState([]);
-  const [query, setQuery] = useState("Batman");
+  const [query, setQuery] = useState("batman");
 
   const fetchMovies = async (url) => {
     setIsLoading(true);
