cbf899eca15e603a68e0aa79b77b50827c166019
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-12 15:19:39 +0100

    Set up movies component

--- src/Movies.js
+++ src/Movies.js
@@ -5,8 +5,29 @@ const url =
   "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  const data = useGlobalContext();
-  return <h2>movies component</h2>;
+  const { movies, isLoading } = useGlobalContext();
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <section className="movies">
+      {movies.map((movie) => {
+        const { imdbID: id, Poster: poster, Title: title, Year: year } = movie;
+        return (
+          <Link to={`/movies/${id}`} key={id} className="movie">
+            <article>
+              <img src={poster} alt={title} />
+              <div className="movie-info">
+                <h4 className="title">{title}</h4>
+                <p>{year}</p>
+              </div>
+            </article>
+          </Link>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Movies;
