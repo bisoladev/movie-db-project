991b3853008a7153425d3f320cbfe6af704d1d90
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-12 15:05:05 +0100

    Fetch API data

--- src/Movies.js
+++ src/Movies.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import { useGlobalContext } from './context'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
 const url =
-  'https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png'
+  "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  return <h2>movies component</h2>
-}
+  const data = useGlobalContext();
+  return <h2>movies component</h2>;
+};
 
-export default Movies
+export default Movies;
--- src/context.js
+++ src/context.js
@@ -5,7 +5,37 @@ console.log(API_ENDPOINT);
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("Batman");
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, [query]);
+
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 // make sure use
 export const useGlobalContext = () => {
