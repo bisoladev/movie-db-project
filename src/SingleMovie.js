5a46f92edfec2f1495f935e07f57977b5958a9a9
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-12 19:22:56 +0100

    Refactored code include useFetch custom hook

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -1,28 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { API_ENDPOINT } from "./context";
+import useFetch from "./useFetch";
 
 const SingleMovie = () => {
   const { id } = useParams();
-  const [movie, setMovie] = useState({});
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: "" });
-
-  const fetchMovie = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data.Response === "False") {
-      setError({ show: true, msg: data.Error });
-      setLoading(false);
-    } else {
-      setMovie(data);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`);
-  }, [id]);
+  const {isLoading, error, data:movie} = useFetch(`&i=${id}`)
 
   if (isLoading) {
     return <div className="loading"></div>;
--- src/context.js
+++ src/context.js
@@ -1,35 +1,12 @@
 import React, { useState, useContext, useEffect } from "react";
 // make sure to use https
+import useFetch from "./useFetch";
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
-console.log(API_ENDPOINT);
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: "" });
-  const [movies, setMovies] = useState([]);
   const [query, setQuery] = useState("batman");
-
-  const fetchMovies = async (url) => {
-    setIsLoading(true);
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-      if (data.Response === "True") {
-        setMovies(data.Search);
-        setError({ show: false, msg: "" });
-      } else {
-        setError({ show: true, msg: data.Error });
-      }
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovies(`${API_ENDPOINT}&s=${query}`);
-  }, [query]);
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`);
 
   return (
     <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
