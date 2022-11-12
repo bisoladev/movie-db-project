5f6c501bd413e726b60f0be735f5dbf159f14973
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-12 13:45:57 +0100

    Set up react router

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="movies/:id" element={<Movie />} />
+      <Route path="movies/:id" element={<Movie />}/>
     </Routes>
   );
 }
