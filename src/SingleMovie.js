f5809f56fa275034478eb0ccb2784fa0a6e8aaa0
Bisola Onaolapo <bisolaonaolapo81@gmail.com>
2022-11-12 16:06:37 +0100

    Fetch data for single movie component

--- src/SingleMovie.js
+++ src/SingleMovie.js
@@ -3,6 +3,38 @@ import { useParams, Link } from "react-router-dom";
 import { API_ENDPOINT } from "./context";
 
 const SingleMovie = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+  const [isLoading, setLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+
+  const fetchMovie = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    if (data.Response === "False") {
+      setError({ show: true, msg: data.Error });
+      setLoading(false);
+    } else {
+      setMovie(data);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovie(`${API_ENDPOINT}&i=${id}`);
+  }, [id]);
+
+  if (isLoading) {
+    return <div className="loading"></div>;
+  }
+  if(error.show) {
+    return <div className="page-error">
+      <h1>{error.msg}</h1>
+      <Link to='/' className="btn">
+        Back to Movies
+      </Link>
+    </div>
+  }
   return <h2>single movie</h2>;
 };
 
